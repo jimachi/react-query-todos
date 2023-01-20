347e8dd4b508cee09e13a8ebdc2f12a4adc2a575
Yuichiro Iijima <jimaichiro@gmail.com>
2023-01-21 02:47:40 +0900

    feat: tanstackの初期設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { FC } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools/build/lib/devtools'
 
-function App() {
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+
+const App: FC = () => {
   return (
-    <div className="">
+    <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 text-sm font-mono">
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter></BrowserRouter>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
