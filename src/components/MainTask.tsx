e42de69a9ee5d3e950de6c8320425aa7aa17b3fd
Yuichiro Iijima <jimaichiro@gmail.com>
2023-01-25 00:02:02 +0900

    feat: タスクの作成/更新/削除を画面から実行できるようにする

--- src/components/MainTask.tsx
+++ src/components/MainTask.tsx
@@ -2,6 +2,7 @@ import React, { FC, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { ChevronDoubleRightIcon } from '@heroicons/react/solid'
 import { TaskList } from './TaskList'
+import { TaskEdit } from './TaskEdit'
 
 export const MainTask: FC = () => {
   const navigate = useNavigate()
@@ -20,11 +21,13 @@ export const MainTask: FC = () => {
       <p className="mb-10 text-xl font-bold">Tasks</p>
       <div className="grid grid-cols-2 gap-40">
         <TaskList />
+        <TaskEdit />
       </div>
       <ChevronDoubleRightIcon
         className="h-5 w-5 mt-2 text-blue-500 cursor-pointer"
         onClick={() => navigate('/tags')}
       />
+      <p>Tag page</p>
     </>
   )
 }
